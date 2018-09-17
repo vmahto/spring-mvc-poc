de32c8c3ac45b48b06ac86e6299f214af90bb105
vmahto <vmahto@securview.com>
2018-09-17 11:24:55 +0530

    Hello From Controller
    Signed-off-by: vmahto <vmahto@securview.com>

--- src/main/java/com/vm/spring/mvc/poc/controller/StudentRegisterationController.java
+++ src/main/java/com/vm/spring/mvc/poc/controller/StudentRegisterationController.java
@@ -1,6 +1,14 @@
 package com.vm.spring.mvc.poc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class StudentRegisterationController {
 
-	
+	@RequestMapping(value = "/hii")
+	public String sayHello() {
+		System.out.println("hii in controller");
+		return "Heloo from Controller";
+	}
 }
